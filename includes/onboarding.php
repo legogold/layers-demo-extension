41c7ea4a8ce1e60d119e8410d9b5f86fef2303d9
vail <vail@obox.co.za>
2015-09-28 21:37:23 +0200

    Onboarding links

--- includes/onboarding.php
+++ includes/onboarding.php
@@ -1,10 +1,12 @@
+
 <section class="layers-area-wrapper">
 
+	
     <div class="layers-onboard-wrapper">
 
         <div class="layers-onboard-controllers">
             <div class="onboard-nav-dots layers-pull-left" id="layers-onboard-anchors"></div>
-            <a class="layers-button btn-link layers-pull-right" href="" id="layers-onboard-skip"><?php _e( 'Skip' , 'layers-storekit' ); ?></a>
+            <a class="layers-button btn-link layers-pull-right" href="" id="layers-onboard-skip"><?php _e( 'Skip' , 'layers-child' ); ?></a>
         </div>
 
         <div class="layers-onboard-slider">
@@ -17,25 +19,29 @@
                         <div class="layers-section-title layers-small layers-no-push-bottom">
                             <div class="layers-push-bottom-small">
                                 <small class="layers-label label-secondary">
-                                    <?php _e( 'StoreKit' , 'layers-storekit' ); ?>
+                                    <?php _e( 'Demo!' , 'layers-child' ); ?>
                                 </small>
                             </div>
                             <h3 class="layers-heading">
-                                <?php _e( 'Thanks for choosing StoreKit!' , 'layers-storekit' ); ?>
+                                <?php _e( 'Thanks for Installing the Child Theme Demo!' , 'layers-child' ); ?>
                             </h3>
                             <div class="layers-excerpt">
                                 <p>
-                                    <?php _e( 'Start by choosing options and settings for Menu Cart, Product List and the Product Page.' , 'layers-storekit' ); ?>
+                                    <?php _e( 'This setup wizard serves as an introduction to your theme and any special options it may have.' , 'layers-child' ); ?>
                                 </p>
+                                <ul>
+                                    <li><a href="http://docs.layerswp.com/plugin-creation-guide/" target="_blank"><?php _e( 'Open the Guide in a New Tab' , 'layers-child' ); ?></a></li>
+                                    <li><a href="http://docs.layerswp.com/how-to-add-help-pages-onboarding-to-layers-themes-or-extensions/" target="_blank"><?php _e( 'Open the Oboarding Tutorial' , 'layers-child' ); ?></a></li>
+                                </ul>    
                             </div>
                         </div>
                     </div>
                     <div class="layers-button-well">
-                        <a class="layers-button btn-primary layers-pull-right onbard-next-step" href=""><?php _e( 'Got it, Next Step &rarr;' , 'layers-storekit' ); ?></a>
+                        <a class="layers-button btn-primary layers-pull-right onbard-next-step" href=""><?php _e( 'Got it, Next Step &rarr;' , 'layers-child' ); ?></a>
                     </div>
                 </div>
                 <div class="layers-column layers-span-8 no-gutter layers-demo-video">
-                    <?php layers_show_html5_video( 'https://s3.amazonaws.com/cdn.oboxsites.com/layers/videos/storekit-toggles.mp4', 660 ); ?>
+                    <img src="<?php echo get_stylesheet_directory_uri(). '/assets/images/help.jpg'; ?>" />
                 </div>
             </div>
 
@@ -47,30 +53,37 @@
                         <div class="layers-section-title layers-small layers-no-push-bottom">
                             <div class="layers-push-bottom-small">
                                 <small class="layers-label label-secondary">
-                                    <?php _e( 'StoreKit' , 'layers-storekit' ); ?>
+                                    <?php _e( 'Demo!' , 'layers-child' ); ?>
                                 </small>
                             </div>
                             <h3 class="layers-heading">
-                                <?php _e( 'Curate your products' , 'layers-storekit' ); ?>
+                                <?php _e( 'Add Video or Images' , 'layers-child' ); ?>
                             </h3>
                             <div class="layers-excerpt">
                                 <p>
-                                    <?php _e( 'StoreKit is packaged with 3 widgets, Product List, Product Slider and Product Categories. Add them to the page just like you would in Layers, to create dynamic layouts with your WooCommerce content.' , 'layers-storekit' ); ?>
+                                    <?php _e( 'Using this wizard template, you can add an HTML5 video (mp4 format) by using the layers_show_html5_video() function in the right column, or simply add a screenshot or graphic illustrating your step. You may also add extended text or HTML in that column, but it must be compatible with the WordPress admin.' , 'layers-child' ); ?>
                                 </p>
+                                 <p>
+                                    <?php _e( 'Basic lists may be used to link your users to offsite resources too, such as your online documentation or companion products.' , 'layers-child' ); ?>
+                                </p>
+                                <ul>
+                                    <li><a href="http://docs.layerswp.com" target="_blank"><?php _e( 'View Our Documentation' , 'layers-child' ); ?></a></li>
+                                    <li><a href="http://docs.layerswp.com/support/" target="_blank"><?php _e( 'Get Support' , 'layers-child' ); ?></a></li>
+                                </ul> 
                             </div>
                         </div>
                     </div>
                     <div class="layers-button-well">
-                        <a class="layers-button btn-primary layers-pull-right" href="<?php echo admin_url( 'customize.php' ); ?>"><?php _e( 'Awesome, Let\'s Go &rarr;' , 'layerswp' ); ?></a>
+                        <a class="layers-button btn-primary layers-pull-right" href="<?php echo admin_url( 'customize.php' ); ?>"><?php _e( 'Launch Customizer &rarr;' , 'layerswp' ); ?></a>
                     </div>
                 </div>
                 <div class="layers-column layers-span-8 no-gutter layers-demo-video">
                     <?php layers_show_html5_video( 'https://s3.amazonaws.com/cdn.oboxsites.com/layers/videos/storekit-widgets.mp4', 660 ); ?>
                 </div>
             </div>
-
-        </div>
+			
+           
+    
 
     </div>
-
 </section>
