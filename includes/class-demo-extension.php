4ed1905e6c1fd292b79c06600cba4dc54aa2b802
vail <vail@obox.co.za>
2015-10-19 15:49:36 +0200

    file location
    moved admin css to assets

--- includes/class-demo-extension.php
+++ includes/class-demo-extension.php
@@ -176,7 +176,7 @@ class Layers_Demo_Extension {
 		
 			wp_enqueue_style(
 				LAYERS_DEMO_EXTENSION_SLUG . '-audio-admin-style',
-				LAYERS_DEMO_EXTENSION_URI . 'css/layers-demo-admin.css',
+				LAYERS_DEMO_EXTENSION_URI . 'assets/css/layers-demo-admin.css',
 				false,
 				LAYERS_DEMO_EXTENSION_VER,
 				'all'
